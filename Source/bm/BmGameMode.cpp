30e3cbbc85c0dad24b0deb213e3d09141b06d6cd
Philipp Welsch <pwelsch86@gmail.com>
2017-11-23 16:39:51 +0100

    Control game setup with BmGameMode
    BmGameMode now serves a few additional purposes.
    
    It has a set of BmInputBindings which can be set via the Editor. This
    InputBindings are distributed to the PlayerControllers based on their
    ControllerID when their Pawn is spawned.
    
    CreatePlayer can be used to set up a player by providing which
    ControllerID it should use. If a controller for that ID does not exist,
    it will be created. Additionally, a pawn will be spawned for each player
    created. For ID 0, the default pawn spawned by the game is used.
    
    BmGameMode also "registers" players by adding them to the tracked
    controllers of BmCameraActor.

--- Source/bm/BmGameMode.cpp
+++ Source/bm/BmGameMode.cpp
@@ -1,25 +1,73 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BmGameMode.h"
+#include "BmPlayerCharacter.h"
+#include "BmPlayerController.h"
+#include "BmCameraActor.h"
 #include "GameFramework/Character.h"
 #include "Kismet/GameplayStatics.h"
 
-void ABmGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+ABmGameMode::ABmGameMode(const FObjectInitializer& ObjectInitializer /*= FObjectInitializer::Get()*/)
+	: Super(ObjectInitializer)
+	, cameraActorInstance(nullptr)
 {
-	Super::InitGame(MapName, Options, ErrorMessage);
+}
+
+APawn* ABmGameMode::SpawnDefaultPawnFor_Implementation(AController* NewPlayer, AActor* StartSpot)
+{
+	ABmPlayerController* bmController = CastChecked<ABmPlayerController>(NewPlayer);
+
+	int32 controllerID = UGameplayStatics::GetPlayerControllerID(bmController);
+	checkf(inputBindings.Num() > controllerID, TEXT("Trying to bind input for controller with ID %d, but only %d Input Bindings exist."), controllerID, inputBindings.Num());
+	bmController->SetInputBindingNames(inputBindings[controllerID]);
+
+	APawn* defaultPlayer = Super::SpawnDefaultPawnFor_Implementation(NewPlayer, StartSpot);
+	bmController->SetupInputComponent();
+
+	return defaultPlayer;
+}
+
+APlayerController* ABmGameMode::SpawnPlayerController(ENetRole InRemoteRole, FVector const& SpawnLocation, FRotator const& SpawnRotation)
+{
+	APlayerController* spawnedController = Super::SpawnPlayerController(InRemoteRole, SpawnLocation, SpawnRotation);
+	return spawnedController;
+}
+
+void ABmGameMode::StartPlay() 
+{
+	Super::StartPlay();
 }
 
 void ABmGameMode::BeginPlay()
 {
 	Super::BeginPlay();
 
-	UGameplayStatics::CreatePlayer(GetWorld(), 0, true);
-	APlayerController* spawnedController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
-	spawnedController->GetCharacter()->AutoPossessPlayer = EAutoReceiveInput::Player0;
+	for (TObjectIterator<ABmCameraActor> iter; iter; ++iter)
+	{
+		if (iter->ActorHasTag("MainCamera"))
+		{
+			cameraActorInstance = *iter;
+		}
+	}
 
-	UGameplayStatics::CreatePlayer(GetWorld(), 1, true);
-	spawnedController = UGameplayStatics::GetPlayerController(GetWorld(), 1);
-	spawnedController->GetCharacter()->AutoPossessPlayer = EAutoReceiveInput::Player0;
+	checkf(cameraActorInstance != nullptr, TEXT("No BmCameraActor marked with MainCamera tag found."));
+
+	CreatePlayer(0);
+	CreatePlayer(1);
+}
+
+void ABmGameMode::CreatePlayer(int32 ControllerID)
+{
+	APlayerController* spawnedController = UGameplayStatics::GetPlayerController(GetWorld(), ControllerID);
+
+	if (spawnedController == nullptr)
+	{
+		spawnedController = UGameplayStatics::CreatePlayer(GetWorld(), ControllerID, true);
+	}
+
+	ABmPlayerCharacter* player = CastChecked<ABmPlayerCharacter>(spawnedController->GetCharacter());
+	player->AutoPossessPlayer = EAutoReceiveInput::Player0;
+	cameraActorInstance->RegisterControllerToTrack(spawnedController);
 }
 
 
--- Source/bm/BmGameMode.h
+++ Source/bm/BmGameMode.h
@@ -4,18 +4,33 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "BmInputBinding.h"
 #include "BmGameMode.generated.h"
 
-/**
- * 
- */
+class ABmCameraActor;
+
 UCLASS()
 class BM_API ABmGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 	
 public:
-	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+	ABmGameMode(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
 	virtual void BeginPlay() override;
+
+	virtual void StartPlay() override;
+
+	APawn* SpawnDefaultPawnFor_Implementation(AController* NewPlayer, AActor* StartSpot) override;
+
+	virtual APlayerController* SpawnPlayerController(ENetRole InRemoteRole, FVector const& SpawnLocation, FRotator const& SpawnRotation) override;
+
+private:
+	void CreatePlayer(int32 ControllerID);
+
+	UPROPERTY(EditAnywhere)
+	TArray<FBmInputBinding> inputBindings;
+
+	UPROPERTY(Transient)
+	ABmCameraActor* cameraActorInstance;
 };
